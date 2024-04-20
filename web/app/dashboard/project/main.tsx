1ec2858fa255669978858af056521e5ec4d04edf
Kanna-Neko <moonandxinxin@gmail.com>
2024-04-20 13:29:05 +0800

    doc: remove doc link from devontainer.json

--- web/app/dashboard/project/main.tsx
+++ web/app/dashboard/project/main.tsx
@@ -38,7 +38,12 @@ export default function Project({project}: {project:projectType}) {
     const create_project: SubmitHandler<CreateProjectForm> = async (data) => {
     }
     return <>
-        <motion.div className="m-2 p-2 h-96 border rounded-md shadow-md" initial={{skewX:-10, opacity:0, y:50}} animate={{skewX:0, opacity:1, y:0}} transition={{duration:0.5}}>
+        <motion.div 
+            className="m-2 p-2 h-96 border rounded-md shadow-md" 
+            initial={{skewX:-10, opacity:0, y:50}} 
+            animate={{skewX:0, opacity:1, y:0}} 
+            transition={{duration:0.5}}
+        >
             <div className="flex justify-end">
                 <motion.button onClick={open_modal} className="rounded-md border text-sm p-1 px-2 shadow-md flex items-center hover:bg-black/10 duration-300" onMouseEnter={() => {set_is_add_hover(true)}} onMouseLeave={()=>{set_is_add_hover(false)}}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
